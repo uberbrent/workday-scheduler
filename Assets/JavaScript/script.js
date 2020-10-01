d71b7edf8323c806655e920562e1f0f5da3d29b5
uberbrent <bmjohns1337@gmail.com>
2020-10-01 10:46:09 -0500

    adding JS file, added rows and columns for main application. Started bootstrap styles

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,18 @@
+// create schedule bones xxx
+    // 9 rows for the hours of the day xxx
+    // 3 columns for the hour/task/save button xxx
+// work on styling
+    // color style
+    // fonts
+    // center header text?
+    // classes to match time of day
+    // colors will match when time has passed or time still remaining
+    // save button icon to lock in task
+// javascript
+    // moment.js to determine time
+    // refresh page every 10 minutes
+    // assign class to determine color in bootstrap
+    // save button fuctionality to lock in text
+    // on click update the task field
+    // save tasks to local storage
+    // display current day at the top of the screen
