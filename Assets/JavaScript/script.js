411cb335a445bc96f62e662019d823ee3cb62f2c
uberbrent <bmjohns1337@gmail.com>
2020-10-01 13:53:49 -0500

    added bootstrap and local css to style, added iconic cdn for icons

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,12 +2,15 @@
     // 9 rows for the hours of the day xxx
     // 3 columns for the hour/task/save button xxx
 // work on styling
-    // color style
+    // color style xxx
     // fonts
-    // center header text?
+    // center header text? xxx
     // classes to match time of day
     // colors will match when time has passed or time still remaining
     // save button icon to lock in task
+// manual css
+    // make proper rounding on save button
+    // padding/margin to time field and save button
 // javascript
     // moment.js to determine time
     // refresh page every 10 minutes
